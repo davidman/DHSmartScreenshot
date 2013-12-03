cd8fc9caaefda29a81fbb6e7669f75a1c66dfd40
David Hernandez <david.hdz@me.com>
2013-12-03 10:45:45 -0600

    Fixed Invalid Context

---
+++ TableViewScreenshots/DHSmartScreenshot/DHSmartScreenshot.h
@@ -0,0 +1,14 @@
+//
+//  DHSmartScreenshot.h
+//  TableViewScreenshots
+//
+//  Created by HERNANDEZ ALVAREZ, DAVID [AG-Contractor/7879] on 12/3/13.
+//  Copyright (c) 2013 David Hernandez. All rights reserved.
+//
+
+#ifndef TableViewScreenshots_DHSmartScreenshot_h
+#define TableViewScreenshots_DHSmartScreenshot_h
+
+#import "UITableView+DHSmartScreenshot.h"
+
+#endif
